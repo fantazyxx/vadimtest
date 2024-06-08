1e229a9b6dc00e342f67abb1204a2465454c2196
Vadim <vo.dcts@gmail.com>
2024-06-08 16:50:42 +0300

    Update styles and fix button functionality

--- view_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Загрузка данных из файла CSV с явным указанием кодировки cp1251 и разделителя
-data = pd.read_csv('Наименование работ.csv', encoding='cp1251', delimiter=';')
-
-# Вывод первых нескольких строк данных и заголовков столбцов
-print(data.head())
-print(data.columns)
-
