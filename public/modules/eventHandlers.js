26ef054dcbff1f3dd7636747b4c016dc7d98531f
Vadim <vo.dcts@gmail.com>
2024-07-12 17:36:02 +0300

    export step12 eventHandlers_2

--- public/modules/eventHandlers.js
+++ public/modules/eventHandlers.js
@@ -1,5 +1,5 @@
-// Обработчик для кнопки "Добавить Акт"
-export function addActButtonClickHandler() {
+// Обработчик для кнопки "Внести Акт"
+export function addActButtonClickHandler(formContainer, menuPage, deviceNumberSelect) {
     console.log('Add Act button clicked');
     menuPage.style.display = 'none';
     formContainer.style.display = 'block';
@@ -7,14 +7,14 @@ export function addActButtonClickHandler() {
   }
   
   // Обработчик для кнопки "Добавить Устройство"
-  export function addDeviceButtonClickHandler() {
+  export function addDeviceButtonClickHandler(menuPage, addDeviceContainer) {
     console.log('Add Device button clicked');
     menuPage.style.display = 'none';
     addDeviceContainer.style.display = 'block';
   }
   
   // Обработчик для кнопки "Назад"
-  export function backButtonClickHandler() {
+  export function backButtonClickHandler(actForm, deviceTypeInput, previousRepairsList, repairListDiv, totalCostInput, repairsToAdd, formContainer, menuPage) {
     console.log('Back button clicked');
     clearForm(actForm, deviceTypeInput, previousRepairsList, repairListDiv, totalCostInput, repairsToAdd);
     formContainer.style.display = 'none';
