ef7c54046203e14a3a8e70ac20935f3cb483a6a3
Vadim <vo.dcts@gmail.com>
2024-07-17 22:24:28 +0300

    workable search DATA att check_finalization_1

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 import { loadDeviceNumbers, loadDeviceType, fetchWorkTypes, updateTotalCost } from './modules/api.js';
 import { clearForm, clearSearch, clearAddDeviceForm, updateRepairsToAdd, populateRegionSelect, createTable } from './modules/utils.js';
-import { addActButtonClickHandler, addDeviceButtonClickHandler, backButtonClickHandler, searchBackButtonClickHandler, addDeviceBackButtonClickHandler, searchDeviceButtonClickHandler, searchButtonClickHandler, clearSearch } from './modules/eventHandlers.js';
+import { addActButtonClickHandler, addDeviceButtonClickHandler, backButtonClickHandler, searchBackButtonClickHandler, addDeviceBackButtonClickHandler, searchDeviceButtonClickHandler, searchButtonClickHandler } from './modules/eventHandlers.js';
 import { searchDeviceRepairs } from './modules/search.js';
 import { handleSubmitActForm } from './modules/form.js';
 
