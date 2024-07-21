2bd06be1fdc23197fe69df8c53906beda5e3df8d
Vadim <vo.dcts@gmail.com>
2024-07-21 19:53:36 +0300

    generate report_api.js_1

--- public/app.js
+++ public/app.js
@@ -1,9 +1,9 @@
-import { loadDeviceNumbers, loadDeviceType, fetchWorkTypes, updateTotalCost } from './modules/api.js';
+import { loadDeviceNumbers, loadDeviceType, fetchWorkTypes, updateTotalCost, generateReport } from './modules/api.js';
 import { clearForm, clearSearch, clearAddDeviceForm, updateRepairsToAdd, populateRegionSelect, createTable } from './modules/utils.js';
 import { addActButtonClickHandler, addDeviceButtonClickHandler, backButtonClickHandler, searchBackButtonClickHandler, addDeviceBackButtonClickHandler, searchDeviceButtonClickHandler, searchButtonClickHandler } from './modules/eventHandlers.js';
+import { generateReportButtonClickHandler, reportBackButtonClickHandler, reportFormSubmitHandler } from './modules/eventHandlers.js';
 import { searchDeviceRepairs } from './modules/search.js';
 import { handleSubmitActForm } from './modules/form.js';
-import { generateReportButtonClickHandler, reportBackButtonClickHandler, reportFormSubmitHandler } from './modules/eventHandlers.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   const addActButton = document.getElementById('add-act-button');
