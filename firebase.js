3fd99d3e2dbdf961e01227981483b1ac3066fc6b
Vadim <vo.dcts@gmail.com>
2024-07-27 15:10:29 +0300

    update index2

--- firebase.js
+++ firebase.js
@@ -1,11 +1,7 @@
 const admin = require('firebase-admin');
 const fs = require('fs');
 
-// Используйте путь к файлу `firebase-config.json`
-const serviceAccount = JSON.parse(fs.readFileSync('./firebase-config.json', 'utf8'));
-
-// Добавьте отладочный вывод
-console.log('Service account loaded:', serviceAccount);
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
